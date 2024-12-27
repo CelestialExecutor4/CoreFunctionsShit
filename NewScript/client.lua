b3a9cc277e103d7a04e7bbc66420f1fee4de43d3
Bullet <creativeteas2+bullet@gmail.com>
2024-12-27 14:47:23 -0500

    Update client.lua

--- NewScript/client.lua
+++ NewScript/client.lua
@@ -6,64 +6,77 @@
 \____/\___/_/\___/____/\__/_/\__,_/_/   
           [ Custom Functions ]                     
 ]]
+getgenv().%CELESTIAL_VERSION% = "shared.Celestial_VERSION"
+getgenv().IS_CELESTIAL_LOADED = false
+getgenv().shared = shared 
+local oldr = request 
+local oldlf = listfiles
+local oldwr = writefile
+local oldghpr = gethiddenproperty
+local camera = workspace.CurrentCamera
+local HttpService, UserInputService, InsertService, CoreGui, RunService = game:FindService("HttpService"), game:FindService("UserInputService"), game:FindService("InsertService"), game:GetService("CoreGui"), game:GetService("RunService")
+local VirtualInputManager = Instance.new("VirtualInputManager")
+shared.httpspy = false
+shared.antilogger = false 
+local hwid = HttpService:GenerateGUID(false)
+local oggm, ogsm = getmetatable, setmetatable
 
--- Define simulated closures and upvalue handling
-do
-    local registry = {}
 
-    -- Simulate upvalues in a closure
-    local function createClosure(func, upvalues)
-        return {
-            func = func,
-            upvalues = upvalues or {}
-        }
-    end
+local renv = {
+	print = print, warn = warn, error = error, shared = shared, assert = assert, collectgarbage = collectgarbage, require = require,
+	select = select, tonumber = tonumber, tostring = tostring, type = type, xpcall = xpcall,
+	pairs = pairs, next = next, ipairs = ipairs, newproxy = newproxy, rawequal = rawequal, rawget = rawget,
+	rawset = rawset, rawlen = rawlen, gcinfo = gcinfo,
 
-    -- Simulate setting a closure value in the registry
-    local function setClosureValue(name, closure)
-        registry[name] = closure
-    end
+	coroutine = {
+		create = coroutine.create, resume = coroutine.resume, running = coroutine.running,
+		status = coroutine.status, wrap = coroutine.wrap, yield = coroutine.yield,
+	},
 
-    -- Simulate getting a closure value from the registry
-    local function getClosureValue(name)
-        return registry[name]
-    end
+	bit32 = {
+		arshift = bit32.arshift, band = bit32.band, bnot = bit32.bnot, bor = bit32.bor, btest = bit32.btest,
+		extract = bit32.extract, lshift = bit32.lshift, replace = bit32.replace, rshift = bit32.rshift, xor = bit32.xor,
+	},
 
-    -- Hook function with upvalue and closure support
-    function hookfunction(original, hook)
-        if type(original) ~= "function" then
-            error("The first argument must be a function (original function).")
-        end
+	math = {
+		abs = math.abs, acos = math.acos, asin = math.asin, atan = math.atan, atan2 = math.atan2, ceil = math.ceil,
+		cos = math.cos, cosh = math.cosh, deg = math.deg, exp = math.exp, floor = math.floor, fmod = math.fmod,
+		frexp = math.frexp, ldexp = math.ldexp, log = math.log, log10 = math.log10, max = math.max, min = math.min,
+		modf = math.modf, pow = math.pow, rad = math.rad, random = math.random, randomseed = math.randomseed,
+		sin = math.sin, sinh = math.sinh, sqrt = math.sqrt, tan = math.tan, tanh = math.tanh
+	},
 
-        if type(hook) ~= "function" then
-            error("The second argument must be a function (hook function).")
-        end
+	string = {
+		byte = string.byte, char = string.char, find = string.find, format = string.format, gmatch = string.gmatch,
+		gsub = string.gsub, len = string.len, lower = string.lower, match = string.match, pack = string.pack,
+		packsize = string.packsize, rep = string.rep, reverse = string.reverse, sub = string.sub,
+		unpack = string.unpack, upper = string.upper,
+	},
 
-        -- Wrap original and hook in closures
-        local originalClosure = createClosure(original)
-        local hookClosure = createClosure(hook)
+	table = {
+		concat = table.concat, insert = table.insert, pack = table.pack, remove = table.remove, sort = table.sort,
+		unpack = table.unpack,
+	},
 
-        -- Store the original function in the registry to prevent GC issues
-        local originalName = "original_" .. tostring(original)
-        setClosureValue(originalName, originalClosure)
+	utf8 = {
+		char = utf8.char, charpattern = utf8.charpattern, codepoint = utf8.codepoint, codes = utf8.codes,
+		len = utf8.len, nfdnormalize = utf8.nfdnormalize, nfcnormalize = utf8.nfcnormalize,
+	},
 
-        -- Replace the original function with the hooked one
-        local hookedFunction = function(...)
-            return hook(original, ...)
-        end
+	os = {
+		clock = os.clock, date = os.date, difftime = os.difftime, time = os.time,
+	},
 
-        local hookedClosure = createClosure(hookedFunction)
+	delay = delay, elapsedTime = elapsedTime, spawn = spawn, tick = tick, time = time, typeof = typeof,
+	UserSettings = UserSettings, version = version, wait = wait, _VERSION = _VERSION,
 
-        -- Store the hooked function in the registry
-        local hookedName = "hooked_" .. tostring(original)
-        setClosureValue(hookedName, hookedClosure)
+	task = {
+		defer = task.defer, delay = task.delay, spawn = task.spawn, wait = task.wait,
+	},
 
-        -- Return the original function reference
-        return function(...)
-            return originalClosure.func(...)
-        end
-    end
+	debug = {
+		traceback = debug.traceback, profilebegin = debug.profilebegin, profileend = debug.profileend, info = debug.info 
+	},
 
-    -- Example of using hookfunction
-    getgenv().hookfunction = hookfunction
-end
+	game = game, workspace = workspace, Game = game, Workspace = workspace,
+
